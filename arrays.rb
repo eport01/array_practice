db4b5fda030b2224aecfceb7f9daf15bb4e2ff1b
Emily Port <emilylport@gmail.com>
2022-08-03 09:20:45 -0600

    Add methods to array

--- arrays.rb
+++ arrays.rb
@@ -2,3 +2,11 @@ animals = ["cat", "dog", "horse", "bird"]
 favorite_numbers = [8, 10, 28, 50]
 decimals = [2.5, 4.8, 8.9, 4.3]
 booleans = [true, false, false, true]
+#The pop method will remove the last element of the array. In this case "bird".
+puts animals.pop
+#The push method will add 45 to the end of the array.
+puts favorite_numbers.push "45"
+#The shift method will remove 2.5 and return it.
+puts decimals.shift
+#The length method will return the number of elements, in this case 4.
+puts booleans.length
