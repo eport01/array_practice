ccce27fa0168a9bb9e4726f19db53e6976bf2686
Emily Port <emilylport@gmail.com>
2022-08-03 09:23:38 -0600

    Add back puts on length method

--- arrays.rb
+++ arrays.rb
@@ -9,4 +9,4 @@ puts favorite_numbers.push "45"
 #The shift method will remove 2.5 and return it.
 puts decimals.shift
 #The length method will return the number of elements, in this case 4.
-booleans.length
+puts booleans.length
